a00c10af0a747ed70ef6a7c8ae8302dd906da06a
lahatra3 <lahatrap20.aps2a@gmail.com>
2023-02-10 10:33:12 +0300

    init project...

---
+++ ranamako/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { 
+    path: '', 
+    loadChildren: () => import('./namana/namana.module').then(m => m.NamanaModule)
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ ranamako/src/app/namana/namana.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NamanaRoutingModule } from './namana-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NamanaRoutingModule
+  ]
+})
+export class NamanaModule { }
